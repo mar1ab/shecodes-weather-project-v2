180e0d0e8b8f1366a606b51ae368dcf2e721e95f
mar1ab <92764905+mar1ab@users.noreply.github.com>
2021-12-11 14:31:26 -0800

    Enabled searching of other cities and display of weather conditions there

--- src/script.js
+++ src/script.js
@@ -21,9 +21,36 @@ function currentCityWeather(response) {
   humidityElement.innerHTML = `Humidity: ${humidity}%`;
   windElement.innerHTML = `Wind: ${wind}km/h`;
 }
+
+function inputCity(event) {
+  event.preventDefault();
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCityInput.value}&units=metric`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(inputCityWeather);
+}
+
+function inputCityWeather(response) {
+  let condition = response.data.weather[0].main;
+  let temp = Math.round(response.data.main.temp);
+  let highTemp = Math.round(response.data.main.temp_max);
+  let lowTemp = Math.round(response.data.main.temp_min);
+  let humidity = Math.round(response.data.main.humidity);
+  let wind = Math.round(response.data.wind.speed);
+  city.innerHTML = `${searchCityInput.value}`;
+  coditionElement.innerHTML = `${condition}`;
+  currentTempElement.innerHTML = `${temp}`;
+  highTempElement.innerHTML = `${highTemp}`;
+  lowTempElement.innerHTML = `${lowTemp}`;
+  humidityElement.innerHTML = `Humidity: ${humidity}%`;
+  windElement.innerHTML = `Wind: ${wind}km/h`;
+}
+
 let apiKey = "c10c120febfbdbb2ecbedb567e2ec32d";
 
 let city = document.querySelector("#city");
+let searchCityInput = document.querySelector("#search-city-input");
+searchCityInput.addEventListener("submit", inputCity);
+let searchCityButton = document.querySelector("#search-city-btn");
+searchCityButton.addEventListener("click", inputCity);
 
 let coditionElement = document.querySelector("#condition");
 let currentTempElement = document.querySelector("#current-temp");
